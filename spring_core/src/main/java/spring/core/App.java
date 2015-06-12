e6cdc66fffe5b22f49401458fa48a0c0786b7825
Lawrence Nag <lawrencenag@gmail.com>
2015-06-12 17:57:40 +0530

    Ritesh baba

--- spring_core/src/main/java/spring/core/App.java
+++ spring_core/src/main/java/spring/core/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello Ritesh Baba!" );
+        System.out.println( "Kya haal hai!" );
     }
 }
