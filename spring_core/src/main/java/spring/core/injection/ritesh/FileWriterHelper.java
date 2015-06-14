abf27b0855186f67eeb535a7b63b49721722d27e
ritesh-spring <ritesh.ejb@gmail.com>
2015-06-14 23:50:33 +0530

    Minor correction

--- spring_core/src/main/java/spring/core/injection/ritesh/FileWriterHelper.java
+++ spring_core/src/main/java/spring/core/injection/ritesh/FileWriterHelper.java
@@ -3,20 +3,29 @@
  */
 package spring.core.injection.ritesh;
 
+
+
 /**
- * @author Ritesh Kumar 
+ * @author Ritesh Kumar
  * @since Jun 14, 2015 1:49:40 AM
  * 
  */
 public class FileWriterHelper {
 
-	IFileWriter fileWriter;
+	 private IFileWriter fileWriter;
+	 
+//	 private List<IFileWriter> fileWriter;
+
+//	public FileWriterHelper(IFileWriter argFileWriter) {
+//		fileWriter = argFileWriter;
+//	}
 
 	public void fileWriter() {
 		// TODO Auto-generated constructor stub
 		fileWriter.writeInFile();
 	}
-
+	
+	
 	public void setFileWriter(IFileWriter fileWriter) {
 		this.fileWriter = fileWriter;
 	}
--- spring_core/src/main/java/spring/core/injection/ritesh/RunFileWriter.java
+++ spring_core/src/main/java/spring/core/injection/ritesh/RunFileWriter.java
@@ -8,20 +8,21 @@ import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * @author Ritesh Kumar 
+ * @author Ritesh Kumar
  * @since Jun 14, 2015 1:56:51 AM
  * 
  */
 public class RunFileWriter {
+
 	public static void main(String[] args) {
-		
+
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"classpath:applicationContext.xml");
 
 		FileWriterHelper writerHelper = (FileWriterHelper) context
 				.getBean("fileWriterHelper");
+
 		writerHelper.fileWriter();
-		
 
 		((AbstractApplicationContext) context).close();
 	}
