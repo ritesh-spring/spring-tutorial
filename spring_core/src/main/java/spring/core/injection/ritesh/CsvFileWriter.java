abca02b75a90db9b7aa47258f0e9baf6f2e8cfc0
aditya-spring <aditya.kec20@gmail.com>
2015-06-15 23:01:27 +0530

    csv file writer logic

--- spring_core/src/main/java/spring/core/injection/ritesh/CsvFileWriter.java
+++ spring_core/src/main/java/spring/core/injection/ritesh/CsvFileWriter.java
@@ -3,6 +3,9 @@
  */
 package spring.core.injection.ritesh;
 
+import java.io.File;
+import java.io.FileWriter;
+
 /**
  * @author Ritesh Kumar 
  * @since   Jun 14, 2015 1:43:53 AM
@@ -15,6 +18,52 @@ public class CsvFileWriter implements IFileWriter {
 	 */
 	public void writeInFile() {
 		System.err.println("Writing in CSV file");
+		try {
+			File csvFileDir = new File("." + File.separator
+					+ "generatedFile");
+
+			File csvFile = new File(csvFileDir.getAbsolutePath()
+					+ File.separator + "myCsvFile.csv");
+
+			if (csvFileDir.exists()) {
+				csvFileDir.delete();
+			}
+			
+			
+			csvFileDir.mkdir();
+			
+			if (csvFile.exists()) {
+				csvFile.delete();
+			}
+
+			csvFile.createNewFile();
+			 FileWriter writer = new FileWriter(csvFile.getAbsolutePath());
+			 
+			    writer.append("DisplayName");
+			    writer.append(',');
+			    writer.append("Age");
+			    writer.append('\n');
+		 
+			    writer.append("MKYONG");
+			    writer.append(',');
+			    writer.append("26");
+		            writer.append('\n');
+		 
+			    writer.append("YOUR NAME");
+			    writer.append(',');
+			    writer.append("29");
+			    writer.append('\n');
+		 
+			    //generate whatever data you want
+		 
+			    writer.flush();
+			    writer.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
+		System.err.println("CSV File Written....Done");
 	}
 
 }
